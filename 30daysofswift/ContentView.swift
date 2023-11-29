74660377c92471fa31f3226e2ac9f090d5385d0b
ducduyn31 <ducduy.ababat@gmail.com>
2023-11-29 17:42:06 +1100

    simple stopwatch

--- 30daysofswift/ContentView.swift
+++ 30daysofswift/ContentView.swift
@@ -1,14 +1,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let timerPublisher = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    @State private var elapsedTime: Double = 0
+    @State private var startTime: Double?
+    @State private var isPlaying: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(format: "%.1f seconds", elapsedTime))
+                .onReceive(timerPublisher, perform: { timer in
+                    if isPlaying {
+                        self.elapsedTime += 0.01
+                    }
+                })
+            
+            Button(action: continueTimer, label: {
+                Text("Play")
+            })
+            .disabled(isPlaying)
+            
+            Button(action: pauseTimer, label: {
+                Text("Pause")
+            }).disabled(!isPlaying)
+            
+            Button(action: resetTimer, label: {
+                Text("Reset")
+            })
         }
         .padding()
+    }
+    
+    private func pauseTimer() {
+        self.isPlaying = false
+    }
+    
+    private func continueTimer() {
+        self.isPlaying = true
+        self.startTime = Date().timeIntervalSince1970
+    }
+    
+    private func resetTimer() {
+        self.isPlaying = false
+        self.elapsedTime = 0
+        
     }
 }
 
