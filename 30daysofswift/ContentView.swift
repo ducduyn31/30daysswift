3ef4e75b0f365b3a8b07ddd4121cc825ddb4c261
ducduyn31 <ducduy.ababat@gmail.com>
2023-12-16 00:17:51 +1100

    click to change font

--- 30daysofswift/ContentView.swift
+++ 30daysofswift/ContentView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedFont = 0
+    private let fonts = ["Avenir", "Helvetica Neue", "Georgia", "Futura", "Menlo"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("\(fonts[selectedFont])")
+                .font(Font.custom(fonts[selectedFont], size: 32))
+            Spacer()
+            Button(action: {
+                selectedFont = (selectedFont + 1) % fonts.count
+            }) {
+                Text("Next Font")
+            }
         }
         .padding()
     }
